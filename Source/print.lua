055394837c5b433063ca71a4f1b4bb4db1b3e097
Xero <124565253+FlexXero@users.noreply.github.com>
2024-08-08 18:23:05 +1200

    Create print.lua

---
+++ Source/print.lua
@@ -0,0 +1,7 @@
+-- some random ahh shit i made
+local function printexecute()
+    local playerName = game.Players.LocalPlayer.Name
+    print(string.format("Hello %s! Your Roblox script has been executed successfully! - Indigo", playerName))
+end
+
+printexecute()
