aed639cdbcb274f8f8d80fff5e1a9521c6e76a96
Xero <124565253+FlexXero@users.noreply.github.com>
2024-08-08 18:20:37 +1200

    Update watermark.lua

--- Source/watermark.lua
+++ Source/watermark.lua
@@ -1,7 +1,7 @@
--- dont think u could remove this, its implemented in injector.
 game.StarterGui:SetCore("SendNotification", {
 Title = "IndigoAPI",
 Text = "Powered By IndigoAPI | .gg/getindigo",
+Icon = "http://www.roblox.com/asset/?id=18842115378",
 Duration = 5,
 })
 setclipboard("https://discord.gg/getindigo")
